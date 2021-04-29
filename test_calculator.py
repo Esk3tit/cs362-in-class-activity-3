4e23891668a72c2d702a65ab45b2547a6b293767
Kai Pan <khaiphn41@gmail.com>
2021-04-29 16:52:11 -0700

    Finished unit tests.

---
+++ test_calculator.py
@@ -0,0 +1,35 @@
+import unittest
+import calculator
+
+
+# Class
+class TestCalc(unittest.TestCase):
+
+    # Test addition
+    def test_add(self):
+        self.assertEqual(calculator.add(1, 1), 2)
+        self.assertEqual(calculator.add(-1, 1), 0)
+        self.assertEqual(calculator.add(1.1, 3.3), 4.4)
+
+    # Test subtraction
+    def test_sub(self):
+        self.assertEqual(calculator.sub(1, 1), 0)
+        self.assertEqual(calculator.sub(-1, 1), -2)
+        self.assertEqual(calculator.sub('1.1', 3.3), 4.4)
+
+    # Test multiplication (first case fails)
+    def test_mult(self):
+        self.assertEqual(calculator.mult(1, 1), 0)
+        self.assertEqual(calculator.mult(0, 1), 0)
+        self.assertEqual(calculator.mult(1.5, 2), 3)
+
+    # Test division (third case fails)
+    def test_div(self):
+        self.assertEqual(calculator.div(1, 1), 1)
+        self.assertEqual(calculator.div(0, 1), 0)
+        self.assertEqual(calculator.div(1, 0), 0)
+        self.assertEqual(calculator.div(1, 0), float("nan"))
+
+
+if __name__ == "__main__":
+    unittest.main()
